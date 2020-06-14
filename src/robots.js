3b90578f330e8b4649fcf294c0ed54dbe7379a9c
Bobve <bobvaneck@gmail.com>
2020-06-14 11:59:08 +1000

    added the project

---
+++ src/robots.js
@@ -0,0 +1,65 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jo Breta',
+        username: 'Bret',
+        email: 'jobreta@gmail.com',
+
+    },
+    {
+        id: 2,
+        name: 'Bo Veck',
+        username: 'Bov',
+        email: 'b.veck@outlook.com',
+
+    },
+    {
+        id: 3,
+        name: 'Christian Christiansson',
+        username: 'CChris',
+        email: 'Chrisson@mail.se',
+
+    },
+    {
+        id: 4,
+        name: 'Pablo Escobar',
+        username: 'Escobar',
+        email: 'Cobra@gmail.com',
+
+    },
+    {
+        id: 5,
+        name: 'Cristoforo Columbus',
+        username: 'Explorer101',
+        email: 'seetheworld@hotmail.it',
+
+    },
+    {
+        id: 6,
+        name: 'd',
+        username: 'd',
+        email: 'd@gmail.com',
+
+    },
+    {
+        id: 7,
+        name: 'a',
+        username: 'a',
+        email: 'a@gmail.com',
+
+    },
+    {
+        id: 8,
+        name: 'B',
+        username: 'B',
+        email: 'b@gmail.com',
+
+    },
+    {
+        id: 9,
+        name: 'c',
+        username: 'c',
+        email: 'c@gmail.com',
+
+    },
+]
